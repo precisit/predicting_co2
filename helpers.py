a39e01b544277f1eb2c3a9996fdb0c64ac221122
Elvira Boman <>
2019-04-30 13:00:24 +0200

    did some moving around functions

--- helpers.py
+++ helpers.py
@@ -5,6 +5,64 @@ from sklearn.metrics import mean_absolute_error as mae
 from sklearn.model_selection import train_test_split as tts
 from sklearn.tree import DecisionTreeRegressor as dtr
 
+from matplotlib.pyplot import *
+
+def get_date():
+	now = datetime.now()
+	year = now.year
+	month = now.month
+	day =  year + month/12 + now.day/365 # decimal day
+	return year, month, day
+
+def get_day():
+	year, month, day = get_date()
+	return day
+
+def get_today():
+	year, month, day = get_date()
+	d = {'Year': [year], 'Month': [month], 'Date': [day]}
+	df = pd.DataFrame(data = d)
+	return df
+
+def get_mae(model, x, y): # val x y
+	pred = model.predict(x)
+	model_mae = mae(pred, y)
+	print("rf:  {:,.6f}".format(model_mae))
+
+def get_axis(x, y, feature):
+	x = x[feature].sort_index(axis = 0)
+	y = y.sort_index(axis = 0)
+	return x, y
+
+def get_plot(X, y, train_X, train_y, val_X, val_y, model_dt, model_rf, model_xgb, axis):
+	xa, ya = get_axis(X, y, axis)
+	xta, yta = get_axis(train_X, train_y, axis)
+	xva, yva = get_axis(val_X, val_y, axis)
+
+	xpa = get_day()
+
+	ya_dt = model_dt.predict(get_today())[0]
+	ya_rf = model_rf.predict(get_today())[0]
+	ya_xgb = model_xgb.predict(get_today())[0]
+
+	subplot(3, 1, 1)
+	xticks([]), yticks([])
+	title('All data')
+	plot(xa, ya) #, xpa, ya_dt, '*', xpa, ya_rf, '*',  xpa, ya_xgb, '*')
+
+	subplot(3, 1, 2)
+	xticks([]), yticks([])
+	title('Training data')
+	plot(xta, yta, 'r-')
+
+	subplot(3, 1, 3)
+	xticks([]), yticks([])
+	title('Validation data')
+	plot(xva, yva, 'g-')
+
+	show()
+
+'''
 def get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y):
     model = dtr(max_leaf_nodes = max_leaf_nodes, random_state = 0)
     model.fit(train_X, train_y)
@@ -12,17 +70,6 @@ def get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y):
     error = mae(val_y, preds_val)
     return(error)
 
-def get_today():
-	now = datetime.now()
-	year = now.year
-	month = now.month
-	day = now.day
-	day_decimal =  year + month/12 + day/365
-	d = {'Year': [year], 'Month': [month], 'Date': [day_decimal]}
-	df = pd.DataFrame(data = d)
-	return(df)
-
-'''
 # compare MAE with differing values of max leaf nodes
 max_leaf_nodes = [5, 50, 500, 5000]
 for i in max_leaf_nodes:
--- predict.py
+++ predict.py
@@ -1,31 +1,33 @@
 import pandas as pd
 from sklearn.metrics import mean_absolute_error as mae
 from sklearn.model_selection import train_test_split as tts
+import numpy as np
+
+from helpers import get_day, get_today, get_plot, get_mae
+from data import get_data
+
+# models
 from sklearn.tree import DecisionTreeRegressor as dtr
 from sklearn.ensemble import RandomForestRegressor as rfr
 from xgboost import XGBRegressor as xgb
-from matplotlib.pyplot import *
-import numpy as np
-
-from helpers import get_today
 
 import warnings
 warnings.filterwarnings("ignore")
-# FIXME här ska man få ladda in ett dataset, bestämma target, bestämma features
-# FIXME para med co2-data, befolkningsöknning, ökad tillväxt, mer?
 
+# variables
+PATH = 'mod.csv'
+FEATURES = ['Year', 'Month', 'Date']
+AXIS = 'Date'
+
+# FIXME para med co2-data, befolkningsöknning, ökad tillväxt, mer?
 print('\n')
 print('--- start ---')
 print('\n')
 
-# perp data
-path = 'mod.csv'
-data = pd.read_csv(path)
-data = data.dropna()
-
-y = data.CO2
-features = ['Year', 'Month', 'Date']
-X = data[features]
+# get data
+data = get_data(PATH)
+y = data.CO2 # set prediction metric
+X = data[FEATURES]
 
 # split to validation and training data
 train_X, val_X, train_y, val_y = tts(X, y, random_state = 1)
@@ -35,23 +37,17 @@ print('validation MAEs')
 # decision tree
 model_dt = dtr(random_state = 1)
 model_dt.fit(train_X, train_y)
-pred_dt = model_dt.predict(val_X)
-mae_dt = mae(pred_dt, val_y)
-print("dt:  {:,.6f}".format(mae_dt))
+get_mae(model_dt, val_X, val_y)
 
 # random forest
 model_rf = rfr(random_state = 1)
 model_rf.fit(train_X, train_y)
-pred_rf = model_rf.predict(val_X)
-mae_rf = mae(pred_rf, val_y)
-print("rf:  {:,.6f}".format(mae_rf))
+get_mae(model_rf, val_X, val_y)
 
 # xgboost
 model_xgb = xgb(random_state = 1, n_estimators = 10000, learning_rate = 0.01)
 model_xgb.fit(train_X, train_y, early_stopping_rounds = 10, eval_set = [(val_X, val_y)], verbose = False)
-pred_xgb = model_xgb.predict(val_X)
-mae_xgb = mae(pred_xgb, val_y)
-print("xgb: {:,.6f}".format(mae_xgb))
+get_mae(model_xgb, val_X, val_y)
 
 # make predictions for today
 user_X = get_today()
@@ -62,29 +58,7 @@ print('rf:  ', model_rf.predict(user_X)[0])
 print('xgb: ', model_xgb.predict(user_X)[0])
 
 # plot
-x_axis = X['Date'].sort_index(axis = 0)
-y_axis = y.sort_index(axis = 0)
-xt_axis = train_X['Date'].sort_index(axis = 0)
-yt_axis = train_y.sort_index(axis = 0)
-xv_axis = val_X['Date'].sort_index(axis = 0)
-yv_axis = val_y.sort_index(axis = 0)
-
-subplot(3, 1, 1)
-xticks([]), yticks([])
-title('All data')
-plot(x_axis, y_axis)
-
-subplot(3, 1, 2)
-xticks([]), yticks([])
-title('Training data')
-plot(xt_axis, yt_axis, 'r-')
-
-subplot(3, 1, 3)
-xticks([]), yticks([])
-title('Validation data')
-plot(xv_axis, yv_axis, 'g-')
-
-show()
+get_plot(X, y, train_X, train_y, val_X, val_y, model_dt, model_rf, model_xgb, AXIS)
 
 print('\n')
 print('done!')
